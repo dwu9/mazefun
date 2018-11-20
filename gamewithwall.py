41bea841d2c19a3e0c1f9f3512f4edf3e8e4409e
dwu9 <dwu@ualberta.ca>
2018-11-20 16:02:23 -0700

    gamewith wall has a class called wall, it runs with no errors, but then the walls don't show up

--- gamewithwall.py
+++ gamewithwall.py
@@ -2,18 +2,26 @@ import pygame as pg
 import picture
 
 
+class wall:
+    def __init__(self, x, y):
+        self.image = pg.Surface((5, 5))
+        self.image.fill((0, 0, 0))
+        self.image.get_rect()
+        self.x = x
+        self.y = y
+        pg.draw.rect(self.image, (0, 0, 0), [x, y, 5, 5])
+
+
 def create_maze(filename):
     screen = pg.display.set_mode((500, 500))
     screen.fill((255, 255, 255))
-    wall = pg.Surface((5, 5))
-    wall.fill((0, 0, 0))
     image = picture.image_to_array(filename)
     height, width = image.shape
     for i in range(0, width):
         for j in range(0, height):
             if image[j][i] == 0:
                 print(f"i see a wall {i}, {j}")
-                pg.draw.rect(screen, (0, 0, 0), [i, j, 5, 5])
+                wall(i, j)
     pg.display.flip()
 
 create_maze("testmaze.png")
