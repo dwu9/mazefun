4fba18f1fcb4dc511b65603229b296062d5d79a6
brendancoutts <bcoutts@ualberta.ca>
2018-11-22 11:47:08 -0700

    Got first split to work. Working on recursion

--- generator.py
+++ generator.py
@@ -12,51 +12,84 @@ class Chamber:
         self.array = array
         self.x = array.shape[0]
         self.y = array.shape[1]
+        if Debug:
+            print(self.x, self.y)
         self.row = row
         self.column = column
 
     def divide(self):
         array = self.array
-        x_line = randint(1, self.x-1)
-        y_line = randint(1, self.y-1)
+        x_line = randint(2, self.x-2)
+        y_line = randint(2, self.y-2)
         if Debug:
-            print(x_line)
-            print(y_line)
-        x_gap = randint(1, self.x-1)
-        y_gap = randint(1, self.y-1)
+            print("x_line " + str(x_line))
+            print("y_line " + str(y_line))
+        x_gap = randint(2, self.x-2)
+        y_gap = randint(2, self.y-2)
         array[x_line, :] = 1
         array[:, y_line] = 1
         array[x_line, y_gap] = 0
         array[x_gap, y_line] = 0
+        if Debug:
+            print(maze.array)
         inner_arrays = []
         # Top left quadrant
-        inner_arrays.append(np.array(x_line - 2, y_line - 2), self.row + 1, self.column + 1)
+        inner_arrays.append(Chamber(np.zeros((x_line - 1, y_line - 1)), self.row + 1, self.column + 1))
         # Top right quadrant
-        inner_arrays.append(np.array(x_line - 2, self.size - y_line - 2), self.row + 1, self.column + y_line + 1)
+        inner_arrays.append(Chamber(np.zeros((x_line - 1, self.y - y_line - 2)), self.row + 1, self.column + y_line + 1))
         # Bottom left quadrant
-        inner_arrays.append(np.array(self.size - x_line - 2, y_line - 2), self.row + x_line + 1, self.pos_y + 1)
+        inner_arrays.append(Chamber(np.zeros((self.x - x_line - 2, y_line - 2)), self.row + x_line + 1, self.column + 1))
         # Bottom right quadrant
-        inner_arrays.append(np.array(x_line - 2, y_line - 2))
-
+        inner_arrays.append(Chamber(np.zeros((y_line - 2, x_line - 2, )), self.row + x_line + 1, self.column + y_line + 1))
+        if Debug:
+            for i in inner_arrays:
+                print(i.array)
+        return inner_arrays
 
 
 
 def create_maze(size):
     # Create a starting blank chamber
     maze = np.zeros((size, size))
-    size = size - 1
+
+
+    return maze
+
+
+def recurse(arraylist):
+    inner_arrays = []
+    for inner_maze in arraylist:
+        if inner_maze.x > 2 and inner_maze.y > 2:
+            children = inner_maze.divide()
+            for i in children:
+                inner_arrays.append(children)
+        else:
+            inner_arrays.append(inner_maze)
+    """
+    if inner_arrays == arraylist:
+        return arraylist
+    else:
+        recurse(inner_arrays)
+    """
+    return inner_arrays
+
+def combine_arrays:
+
+
+def create_boarder(array):
+    # Change this to append and create a boarder
+
     maze[0, :] = 1
     maze[:, 0] = 1
     maze[size, :] = 1
     maze[:, size] = 1
-    return maze
-
 
 maze = Chamber(create_maze(20), 0, 0)
-maze.divide()
-print(maze.array)
+#print(maze.array)
+inner_arrays = maze.divide()
+children = recurse(inner_arrays)
 # I need you to work on this image conversion:
-image = Image.fromarray(maze.array, '1')
-image.save('genmaze.png')
+#image = Image.fromarray(maze.array, '1')
+#image.save('genmaze.png')
 #image.show()
 
