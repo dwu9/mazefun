de2f2c5d38eada3f3aa890e6feb3148d42163ccd
brendancoutts <bcoutts@ualberta.ca>
2018-11-21 20:38:30 -0700

    Changed algorithm movement is working but maze is kinda glitchy

--- game.py
+++ game.py
@@ -3,11 +3,12 @@ import picture
 
 
 class Wall:
-    def __init__(self, x, y):
+    def __init__(self, x, y, rect):
         self.image = pg.display.get_surface()
+        self.rect = rect
         self.x = x
         self.y = y
-        pg.draw.rect(self.image, (0, 0, 0), [x, y, 2, 2])
+        pg.draw.rect(self.image, (0, 0, 0), [x, y, 1, 1])
 
 
 class Player:
@@ -17,6 +18,7 @@ class Player:
         self.rect = rect
         self.surface = pg.Surface(self.rect.size)
         self.image = pg.draw.rect(self.screen, self.color, self.rect)
+
         # pg.draw.rect(self.screen, self.color, self.rect)
 
     def update(self):
@@ -24,6 +26,7 @@ class Player:
 
     def clear(self):
         pg.draw.rect(self.screen, (255, 255, 255), self.rect)
+
 
 
 def create_maze(filename):
@@ -36,8 +39,8 @@ def create_maze(filename):
     for i in range(0, width):
         for j in range(0, height):
             if image[j][i] < 70:
-                wall = Wall(i, j)
-                walls.append(wall)
+                wall = Wall(i, j, pg.Rect(i, j, 1, 1))
+                walls.append(wall.rect)
                 walllist.append((i, j))
     pg.display.flip()
     print(walllist)
@@ -45,46 +48,64 @@ def create_maze(filename):
 
 
 def run_maze():
-    screen, walllist, walls = create_maze("testmaze1.png")
+    screen, walllist, walls = create_maze("testmazemedium.png")
     player = Player(screen, pg.rect.Rect(15, 5, 5, 5))
     pg.key.set_repeat(30, 30)
     running = True
+    print(type(walls[0]))
     while running:
         for event in pg.event.get():
             if event.type == pg.VIDEORESIZE:
                 xsize, ysize = event.dict['size'][0], event.dict['size'][1]
                 print (xsize, ysize)
             if event.type == pg.KEYDOWN:
-                if event.key == pg.K_a:
-                    if (player.rect.left - 1, player.rect.top) in walllist:
-                        pass
-                    else:
-                        player.clear()
-                        player.rect.move_ip(-1, 0)
-                        player.update()
-
-                if event.key == pg.K_d:
-                    if (player.rect.left + 6, player.rect.top) in walllist:
-                        pass
-                    else:
-                        player.clear()
+                if event.key == pg.K_a: # A to move left
+                    player.clear()
+                    player.rect.move_ip(-1, 0)
+                    if  player.rect.collidelist(walls) != -1:
+                        #player.clear()
                         player.rect.move_ip(1, 0)
                         player.update()
-
-                if event.key == pg.K_s:
-                    if (player.rect.left, player.rect.top + 6) in walllist:
                         pass
                     else:
                         player.clear()
-                        player.rect.move_ip(0, 1)
+                        #player.rect.move_ip(-1, 0)
                         player.update()
 
-                if event.key == pg.K_w:
-                    if (player.rect.left, player.rect.top - 1) in walllist:
-                        pass
+                if event.key == pg.K_d: # D to move right
+                    player.clear()
+                    player.rect.move_ip(1, 0)
+                    if player.rect.collidelist(walls) != -1:
+                        #player.clear()
+                        player.rect.move_ip(-1, 0)
+                        player.update()
                     else:
-                        player.clear()
+                        #player.clear()
+                        #player.rect.move_ip(1, 0)
+                        player.update()
+
+                if event.key == pg.K_s: # S to move down
+                    player.clear()
+                    player.rect.move_ip(0, 1)
+                    if player.rect.collidelist(walls) != -1:
+                        # player.clear()
                         player.rect.move_ip(0, -1)
+                        player.update()
+                    else:
+                        #player.clear()
+                        # player.rect.move_ip(0, 1)
+                        player.update()
+
+                if event.key == pg.K_w: # W to move up
+                    player.clear()
+                    player.rect.move_ip(0, -1)
+                    if player.rect.collidelist(walls) != -1:
+                        # player.clear()
+                        player.rect.move_ip(0, 1)
+                        player.update()
+                    else:
+                        # player.clear()
+                        # player.rect.move_ip(0, -1)
                         player.update()
 
             if event.type == pg.QUIT:
