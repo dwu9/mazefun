91ceb74f73879cc5d8674fd3fb46409ebba2b234
dwu9 <dwu@ualberta.ca>
2018-11-28 15:59:22 -0700

    I made prim generator

--- prim_generatorv2.py
+++ prim_generatorv2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random as rand
 import matplotlib.pyplot as pyplot
+import sys
 
 
 def create_maze(size):
@@ -18,12 +19,11 @@ def draw_maze(maze):
 def release_kraken(maze, frontier_list, checked_list):
     row_index = maze.shape[0] - 1
     column_index = maze.shape[1] - 1
-    start_row = rand.randint(0, row_index)
-    start_column = rand.randint(0, column_index)
+    start_row = rand.randint(1, row_index - 1)
+    start_column = rand.randint(1, column_index - 1)
     frontier_list.append((start_row, start_column))
     checked_list.append((start_row, start_column))
     maze[start_row][start_column] = 0
-    return maze, frontier_list
 
 
 def prim_gen(maze, frontier_list, checked_list):
@@ -38,33 +38,40 @@ def prim_gen(maze, frontier_list, checked_list):
     upper_left = (start_row + 1, start_column - 1)
     lower_right = (start_row - 1, start_column + 1)
     lower_left = (start_row - 1, start_column - 1)
-    direction_list = [top, bottom, left, right]
-    cross = [maze[top], maze[bottom], maze[left], maze[right], maze[upper_left], maze[upper_right], maze[lower_left], maze[lower_right]]
+    try:
+        cross = [maze[top], maze[bottom], maze[left], maze[right], maze[upper_left], maze[upper_right], maze[lower_left], maze[lower_right]]
+    except IndexError:
+        print('ERROR')
+        print(start_row, start_column)
+        sys.exit(-1)
     if start_row in range(1, row_index - 1):
         if start_column in range(1, column_index - 1):
-            print(sorted(cross))
+            # print(sorted(cross))
             if sorted(cross) == [False, False, True, True, True, True, True, True] or sorted(cross) == [False, True, True, True, True, True, True, True] or sorted(cross) == [True, True, True, True, True, True, True, True]:
                 maze[start_row][start_column] = 0
                 if top not in frontier_list and top not in checked_list:
                     frontier_list.append(top)
-                if bottom not in frontier_list and top not in checked_list:
+                if bottom not in frontier_list and bottom not in checked_list:
                     frontier_list.append(bottom)
-                if left not in frontier_list and top not in checked_list:
+                if left not in frontier_list and left not in checked_list:
                     frontier_list.append(left)
-                if right not in frontier_list and top not in checked_list:
+                if right not in frontier_list and right not in checked_list:
                     frontier_list.append(right)
-        # frontier_list.remove((start_row, start_column))
         checked_list.append((start_row, start_column))
-    return maze, frontier_list, checked_list, flag
 
 
-# Start with a grid full of walls.
-maze = create_maze(20)
+maze = create_maze(30)
 frontier_list = []
 checked_list = []
-maze, frontier_list = release_kraken(maze, frontier_list, checked_list)
-maze, frontier_list, checked_list = prim_gen(maze, frontier_list, checked_list)
-while something:
-    prim_gen(maze, frontier_list, checked_list)
+release_kraken(maze, frontier_list, checked_list)
 
+# while checked_list != frontier_list or run == 0:
+for i in range(0, 20000):
+    prim_gen(maze, frontier_list, checked_list)
+maze = np.delete(maze, 29, 0)
+maze = np.delete(maze, 29, 1)
+maze[10][0] = 0
+maze[10][28] = 0
+maze[11][0] = 0
+maze[11][28] = 0
 draw_maze(maze)
