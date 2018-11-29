2228f805c6613abb7e4c8a6fad26f6ae1c1b079d
dwu9 <dwu@ualberta.ca>
2018-11-29 16:09:50 -0700

    It now runs with a while

--- prim_generatorv4.py
+++ prim_generatorv4.py
@@ -17,29 +17,37 @@ def draw_maze(maze):
     pp.show()
 
 
-def release_kraken(maze, frontier_list, checked_list):
+def release_kraken(maze, frontier_list):
     row_index = maze.shape[0] - 1
     column_index = maze.shape[1] - 1
-    start_row = rand.randint(1, row_index - 2)
-    start_column = rand.randint(1, column_index - 2)
+    start_row = rand.randint(0, row_index)
+    start_column = rand.randint(0, column_index)
     frontier_list.append((start_row, start_column))
-    checked_list.append((start_row, start_column))
-    maze[start_row][start_column] = 0
 
 
-def nearby_blocks(start_row, start_column):
-    global size
-    for i in [top, bottom, left, right, upper_left, upper_right, lower_left, lower_right]:
-        i = None
-    if (start_row + 2) < size - 1:
-        top = (start_row + 2, start_column)
-    if (start_row - 2) > 0:
-        bottom = (start_row - 2, start_column)
-    if (start_column - 2) > 0:
-        left = (start_row, start_column - 2)
-    if (start_column + 2) < size - 1:
-        right = (start_row, start_column + 2)
-    return top, bottom, left, right
+def far_neighbours(start_row, start_column, n):
+    far_neighbour_list = []
+    if start_row + n <= maze_size - 1:
+        far_top = (start_row + n, start_column)
+    else:
+        far_top = None
+    far_neighbour_list.append(far_top)
+    if start_column + n <= maze_size - 1:
+        far_right = (start_row, start_column + n)
+    else:
+        far_right = None
+    far_neighbour_list.append(far_right)
+    if start_column - n >= 1:
+        far_left = (start_row, start_column - n)
+    else:
+        far_left = None
+    far_neighbour_list.append(far_left)
+    if start_row - n >= 1:
+        far_bottom = (start_row - 2, start_column)
+    else:
+        far_bottom = None
+    far_neighbour_list.append(far_bottom)
+    return far_neighbour_list, far_top, far_right, far_left, far_bottom
 
 
 def frontier_prep(maze, frontier_list, checked_list, direction):
@@ -47,43 +55,72 @@ def frontier_prep(maze, frontier_list, checked_list, direction):
         frontier_list.append(direction)
 
 
-def prim_gen(maze, frontier_list, checked_list):
-    row_index = maze.shape[0] - 1
-    column_index = maze.shape[1] - 1
+def prim_gen(maze, frontier_list):
     start_row, start_column = rand.choice(frontier_list)
-    top, bottom, left, right, upper_left, upper_right, lower_left, lower_right = nearby_blocks(start_row, start_column)
-    cross = [maze[top], maze[bottom], maze[left], maze[right]]
-    if start_row in range(1, row_index - 2):
-        if start_column in range(1, column_index - 2):
-            if sorted(cross) == [0, 1, 1, 1] or sorted(cross) == [1, 1, 1, 1]:
-                maze[start_row][start_column] = 0
-                for direction in [top, left, right, bottom]:
-                    if direction[0] != 0 and direction[0] != maze_size - 2 and direction[1] != maze_size - 2:
-                        frontier_prep(maze, frontier_list, checked_list, direction)
-        checked_list.append((start_row, start_column))
-        frontier_list.remove((start_row, start_column))
+    far_neighbours_list, far_top, far_right, far_left, far_bottom = far_neighbours(start_row, start_column, 2)
+    neighbours_list, top, right, left, bottom = far_neighbours(start_row, start_column, 1)
+
+    while far_neighbours_list:
+        '''
+        print(far_neighbours_list)
+        print('Far left is ' + str(far_left))
+        print('Far right is ' + str(far_right))
+        print('Far top is ' + str(far_top))
+        print('Far bottom is ' + str(far_bottom))
+        '''
+        if far_top is not None:
+            if maze[far_top] == 1:
+                maze[top] = 0
+                neighbours_list1, top1, right1, left1, bottom1 = far_neighbours(far_top, 1)
+                for i in neighbours_list1:
+                    if i is not None:
+                        frontier_list.append(i)
+        far_neighbours_list.remove(far_top)
+
+        if far_right is not None:
+            if maze[far_right] == 1:
+                maze[right] = 0
+                neighbours_list2, top2, right2, left2, bottom2 = far_neighbours(far_right, 1)
+                for i in neighbours_list2:
+                    if i is not None:
+                        frontier_list.append(i)
+        far_neighbours_list.remove(far_right)
+
+        if far_left is not None:
+            if maze[far_left] == 1:
+                maze[left] = 0
+                neighbours_list3, top3, right3, left3, bottom3 = far_neighbours(far_left, 1)
+                for i in neighbours_list3:
+                    if i is not None:
+                        frontier_list.append(i)
+        far_neighbours_list.remove(far_left)
+
+        if far_bottom is not None:
+            if maze[far_bottom] == 1:
+                maze[bottom] = 0
+                neighbours_list4, top4, right4, left4, bottom4 = far_neighbours(far_bottom, 1)
+                for i in neighbours_list4:
+                    if i is not None:
+                        frontier_list.append(i)
+        far_neighbours_list.remove(far_bottom)
+
+    frontier_list.remove((start_row, start_column))
+
 
 
 def main():
     maze = create_maze(maze_size)
     frontier_list = []
-    checked_list = []
-    release_kraken(maze, frontier_list, checked_list)
     run = 0
+    release_kraken(maze, frontier_list)
 
     # while checked_list != frontier_list or run == 0:
-    while frontier_list:
-        prim_gen(maze, frontier_list, checked_list)
+    for i in range(0, 20):
+        prim_gen(maze, frontier_list)
         run += 1
         #draw_maze(maze)
-    maze = np.delete(maze, maze_size - 1, 0)
-    maze = np.delete(maze, maze_size - 1, 1)
-    maze[5][0] = 0
-    maze[maze_size - 5][maze_size - 2] = 0
-    maze[5][1] = 0
-    maze[maze_size - 5][maze_size - 3] = 0
     draw_maze(maze)
-    print(run)
+    #print(run)
 
 
 main()
